947df685ec7dbb321bf3534d296f324b5e19f4ac
yoichi kobayashi <info@tplh.net>
2015-12-29 22:12:32 +0900

    [add] task serve.

---
+++ gulp/conf.js
@@ -0,0 +1,21 @@
+// 設定ファイル
+// 対象パスやオプションを指定
+
+const DIR = module.exports.DIR = {
+  PATH: '',
+  SRC: 'src',
+  DST: 'dst'
+};
+
+module.exports.serve = {
+  notify: false,
+  startPath: `${DIR.PATH}`,
+  ghostMode: false,
+  server: {
+    baseDir: DIR.DEST,
+    index: 'index.html',
+    routes: {
+      [DIR.PATH]: `${DIR.DEST}${DIR.PATH}/`
+    }
+  }
+};
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const requireDir = require('require-dir');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+requireDir('./gulp/tasks');
