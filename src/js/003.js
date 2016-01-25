bf39a2a28134b60b7ddaee69666450fee5e88e57
yoichi kobayashi <info@tplh.net>
2016-01-25 20:26:06 +0900

    [WIP] 立方体のvertex、color、indexを指定。

--- src/js/003.js
+++ src/js/003.js
@@ -12,29 +12,65 @@ const gl = canvas.getContext('webgl');
 const mMatrix = mat4.identity(mat4.create());
 const vMatrix = mat4.identity(mat4.create());
 const pMatrix = mat4.identity(mat4.create());
-const vertices1 = [
-  -0.5, 0.4, 0,
-  -0.3, 0.6, 0,
-  -0.1, 0.4, 0,
-  0.1, 0.6, 0,
-  0.3, 0.4, 0,
-  0.5, 0.6, 0,
+const vertices = [
+  // Front face
+  -1.0, -1.0,  1.0,
+   1.0, -1.0,  1.0,
+   1.0,  1.0,  1.0,
+  -1.0,  1.0,  1.0,
+
+  // Back face
+  -1.0, -1.0, -1.0,
+  -1.0,  1.0, -1.0,
+   1.0,  1.0, -1.0,
+   1.0, -1.0, -1.0,
+
+  // Top face
+  -1.0,  1.0, -1.0,
+  -1.0,  1.0,  1.0,
+   1.0,  1.0,  1.0,
+   1.0,  1.0, -1.0,
+
+  // Bottom face
+  -1.0, -1.0, -1.0,
+   1.0, -1.0, -1.0,
+   1.0, -1.0,  1.0,
+  -1.0, -1.0,  1.0,
+
+  // Right face
+   1.0, -1.0, -1.0,
+   1.0,  1.0, -1.0,
+   1.0,  1.0,  1.0,
+   1.0, -1.0,  1.0,
+
+  // Left face
+  -1.0, -1.0, -1.0,
+  -1.0, -1.0,  1.0,
+  -1.0,  1.0,  1.0,
+  -1.0,  1.0, -1.0
 ];
-const vertices2 = [
-  -0.5, -0.1, 0,
-  -0.3, 0.1, 0,
-  -0.1, -0.1, 0,
-  0.1, 0.1, 0,
-  0.3, -0.1, 0,
-  0.5, 0.1, 0,
+const colors = [
+  [1.0,  1.0,  1.0,  1.0],    // Front face: white
+  [1.0,  0.0,  0.0,  1.0],    // Back face: red
+  [0.0,  1.0,  0.0,  1.0],    // Top face: green
+  [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+  [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+  [1.0,  0.0,  1.0,  1.0]     // Left face: purple
 ];
-const vertices3 = [
-  -0.5, -0.6, 0,
-  -0.3, -0.4, 0,
-  -0.1, -0.6, 0,
-  0.1, -0.4, 0,
-  0.3, -0.6, 0,
-  0.5, -0.4, 0,
+let generated_colors = [];
+for (let j = 0; j < 6; j++) {
+  let c = colors[j];
+  for (let i = 0; i < 4; i++) {
+    generated_colors = generated_colors.concat(c);
+  }
+}
+const indices = [
+  0,  1,  2,      0,  2,  3,    // front
+  4,  5,  6,      4,  6,  7,    // back
+  8,  9,  10,     8,  10, 11,   // top
+  12, 13, 14,     12, 14, 15,   // bottom
+  16, 17, 18,     16, 18, 19,   // right
+  20, 21, 22,     20, 22, 23    // left
 ];
 
 const init = () => {
@@ -46,22 +82,29 @@ const init = () => {
 
   const program = loadProgram(gl, glslify('./003.vs'), glslify('./003.fs'));
   const attr_position = gl.getAttribLocation(program, 'position');
+  const attr_color = gl.getAttribLocation(program, 'color');
+  const attr_index = gl.getAttribLocation(program, 'index');
+  gl.enableVertexAttribArray(attr_position);
+  gl.enableVertexAttribArray(attr_color);
+  gl.enableVertexAttribArray(attr_index);
+
   const vertex_buffer = gl.createBuffer();
+  const color_buffer = gl.createBuffer();
+  const index_buffer = gl.createBuffer();
+
   gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generated_colors), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, index_buffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices1), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
   gl.vertexAttribPointer(attr_position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(attr_position);
-  gl.drawArrays(gl.LINE_LOOP, 0, vertices1.length / 3);
+  gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+  gl.vertexAttribPointer(attr_color, 4, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(attr_position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(attr_position);
-  gl.drawArrays(gl.LINES, 0, vertices2.length / 3);
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices3), gl.STATIC_DRAW);
-  gl.vertexAttribPointer(attr_position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(attr_position);
-  gl.drawArrays(gl.LINE_STRIP, 0, vertices3.length / 3);
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3);
 };
 init();
