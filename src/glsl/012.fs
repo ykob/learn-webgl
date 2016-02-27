9c8f4067c20dde490c117c1d0f6a0786cb31ff1d
yoichi kobayashi <info@tplh.net>
2016-02-27 11:40:41 +0900

    clean up.

--- src/glsl/012.fs
+++ src/glsl/012.fs
@@ -4,22 +4,18 @@ precision mediump float;
 
 uniform mat4 inv_matrix;
 uniform vec3 light_direction;
-uniform float time;
 uniform sampler2D texture;
 
 varying vec3 vPosition;
 varying vec3 vColor;
 varying vec2 vUv;
 
-#pragma glslify: hsv2rgb = require(./module/hsv2rgb)
-
 void main() {
-  vec3 custom_color = hsv2rgb(vColor);
   vec3 dx = dFdx(vPosition.xyz);
   vec3 dy = dFdy(vPosition.xyz);
   vec3 n = normalize(cross(normalize(dx), normalize(dy)));
   vec3 inv_light = normalize(inv_matrix * vec4(light_direction, 1.0)).xyz;
   float diff = clamp(dot(n, inv_light), 0.1, 1.0);
   vec4 smp_color = texture2D(texture, vUv);
-  gl_FragColor = vec4(custom_color * diff, 1.0) * smp_color;
+  gl_FragColor = vec4(vColor * diff, 1.0) * smp_color;
 }
--- src/glsl/012.vs
+++ src/glsl/012.vs
@@ -2,7 +2,6 @@ precision mediump float;
 
 attribute vec3 position;
 attribute vec3 color;
-attribute vec3 normal;
 attribute vec2 uv;
 
 uniform mat4 p_matrix;
@@ -12,9 +11,11 @@ varying vec3 vPosition;
 varying vec3 vColor;
 varying vec2 vUv;
 
+#pragma glslify: hsv2rgb = require(./module/hsv2rgb)
+
 void main() {
   vPosition = position;
-  vColor = color;
+  vColor = hsv2rgb(color);
   vUv = uv;
 
   vec4 mv_position = mv_matrix * vec4(position, 1.0);
