f155570b7e43f6b03b8ef944da60fe9656be1146
Yoichi Kobayashi <info@tplh.net>
2016-03-31 02:55:28 +0900

    [modify] noize pattern update.

--- src/glsl/014.fs
+++ src/glsl/014.fs
@@ -28,11 +28,11 @@ float dsBox(vec3 p, vec3 size, float v) {
 }
 
 float distanceFunc(vec3 p) {
-  float n1 = snoise3(p * 0.3 + time / 200.0);
+  float n1 = snoise3(p * 0.26 + time / 100.0);
   vec3 p1 = rotate(p, radians(time), radians(time * 2.0), radians(time));
   float d1 = dsBox(p1, vec3(2.0), 0.5);
-  float d2 = dSphere(p1, 3.2) - n1;
-  float d3 = dSphere(p1, 1.8) - n1;
+  float d2 = dBox(p1, vec3(2.4)) - n1;
+  float d3 = dBox(p1, vec3(1.95)) - n1;
   return min(max(d1, -d2), d3);
 }
 
@@ -62,20 +62,20 @@ void main() {
   for(int i = 0; i < 64; i++){
       distance = distanceFunc(rPos);
       rLen += distance;
-      rPos = cPos + ray * rLen * 0.1;
+      rPos = cPos + ray * rLen * 0.2;
   }
 
   vec3 normal = getNormal(rPos);
-  if(abs(distance) < 1.0){
-    float n = snoise3(rPos * 0.3 + time / 200.0);
-    vec3 p = rotate(rPos, radians(time * -2.0), radians(time * 2.0), radians(time * -2.0));
-    float d = dSphere(p, 1.6) - n;
-    if (d > 1.0) {
+  if(abs(distance) < 0.5){
+    float n = snoise3(rPos * 0.26 + time / 100.0);
+    vec3 p = rotate(rPos, radians(time), radians(time * 2.0), radians(time));
+    float d = dBox(p, vec3(1.85)) - n;
+    if (d > 0.5) {
       gl_FragColor = vec4(hsv2rgb(vec3(dot(normal, cUp) * 0.8 + time / 200.0, 0.2, dot(normal, cUp) * 0.8 + 0.1)), 1.0);
     } else {
       gl_FragColor = vec4(hsv2rgb(vec3(dot(normal, cUp) * 0.1 + time / 200.0, 0.8, dot(normal, cUp) * 0.2 + 0.8)), 1.0);
     }
-  }else {
+  } else {
     gl_FragColor = vec4(0.9, 0.9, 0.9, 1.0);
   }
 }
